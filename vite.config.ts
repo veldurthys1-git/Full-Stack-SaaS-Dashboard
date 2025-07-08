168e17642b5f9a6089d9e28e9ca7a3a6217e3b96
Charan <veldurthysaicharan5@gmail.com>
2025-07-08 11:31:53 -0700

    Fix package.json and add deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Full-Stack-SaaS-Dashboard/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
